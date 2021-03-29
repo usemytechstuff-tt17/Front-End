eed885d27054aeeb91bf31fb5541f9403e5081d0
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-29 12:41:42 -0700

    nav laid out and Private Route.

---
+++ use-my-tech-stuff/src/components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+return(
+
+    <div className='navbar'>
+        <img src='' alt='' />
+        <p>Connecting people in need to people with Greed!</p>
+
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Creat Accoutn</Link>
+                </li>
+                /
+                <li>
+                    <Link to=''>Log-in</Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+
+)
