fb8eee917e793ee14d39e5158b81902576d7ab11
Francis Nguyen <francishtknguyen@gmail.com>
2021-03-29 19:19:37 -0500

    changed lis

--- use-my-tech-stuff/src/components/ListingCard.js
+++ use-my-tech-stuff/src/components/ListingCard.js
@@ -1,34 +1,34 @@
 import React,{ useState, useEffect }  from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+// import axiosWithAuth from '../utils/axiosWithAuth';
 
 
-const initialData = {
+const initialData = [{
     name: 'Uber cool camera',
     price: 10000,
     details: 'My one of a kind kodak disposable camera',
-}
+}]
 
-const ListingCard =() => {
+const ListingCard =(props) => {
 
-    const [data, setData] = useState(initialData);
+    // const [data, setData] = useState(initialData);
     
-    useEffect(() => {
-        axiosWithAuth().get('/items')
-        .then( res => {
-            console.log(res)
-            setData(res.data)
-        })
-        .catch((err) => {
-            console.log(err.response)
-        })
-    },[])
+    // useEffect(() => {
+    //     axiosWithAuth().get('/items')
+    //     .then( res => {
+    //         console.log(res)
+    //         setData(res.data)
+    //     })
+    //     .catch((err) => {
+    //         console.log(err.response)
+    //     })
+    // },[])
 
     return(
         <div>
-        <h2>{data.name}</h2>
+        <h2>{props.name}</h2>
         <p>img?</p>
-        <p>${data.price}/per day</p>
-        <p>Details: {data.details}</p>
+        <p>${props.price}/per day</p>
+        <p>Details:{props.details}</p>
         </div>
     )
 }
