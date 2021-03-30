bc36a7d84df6e04761d4b98ee4b70b86861e8034
Chris Burton <chris@fullstackcaveman.com>
2021-03-30 11:29:43 -0600

    Initial setup of contextApi - refactored Index/App to work with context better

--- use-my-tech-stuff/src/index.js
+++ use-my-tech-stuff/src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PrivateRoute from './utils/PrivateRoute';
-import LoginForm from './components/Login';
-
-import Create from './components/createuser';
-import Home from './components/Home';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<Router>
-		<App/>
-		<Switch>
-			<Route path='/login' component={LoginForm} />
-			<Route path='/register' component={Create} />
-			<Route path='/' component={Home}/>
-		</Switch>
+		<App />
 	</Router>,
 	document.getElementById('root')
 );
