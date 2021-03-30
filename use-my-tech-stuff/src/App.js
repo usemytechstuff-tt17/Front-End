e8a9110a7a45bb19b3933d1b1d5987567493daf7
Chris Burton <chris@fullstackcaveman.com>
2021-03-30 13:52:24 -0600

    Initial setup of user logged in state

--- use-my-tech-stuff/src/App.js
+++ use-my-tech-stuff/src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Route, Switch } from 'react-router-dom';
 import { TechContext } from './contexts/techContext';
+import { UserContext } from './contexts/userContext';
 
 import Create from './components/createuser';
 import Home from './components/Home';
@@ -8,10 +10,26 @@ import LoginForm from './components/Login';
 import Nav from './components/Nav';
 
 import './App.css';
-import axios from 'axios';
 
 function App() {
 	const [tech, setTech] = useState([]);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	// Quick way to set state without knowing what localStorage item will be called - may change
+	useEffect(() => {
+		if (!localStorage.token) {
+			localStorage.setItem('token', '');
+		}
+
+		const userStatus = localStorage.getItem('token');
+		if (!userStatus) {
+			setIsLoggedIn(false);
+		} else {
+			setIsLoggedIn(true);
+		}
+	}, [isLoggedIn]);
+
+	console.log(isLoggedIn);
 
 	useEffect(() => {
 		axios
@@ -27,13 +45,15 @@ function App() {
 	return (
 		<div className='app'>
 			<TechContext.Provider value={{ tech }}>
-				<Nav />
-				<h1>Use My Tech Stuff</h1>
-				<Switch>
-					<Route path='/login' component={LoginForm} />
-					<Route path='/register' component={Create} />
-					<Route path='/' component={Home} />
-				</Switch>
+				<UserContext.Provider value={{ isLoggedIn }}>
+					<Nav />
+					<h1>Use My Tech Stuff</h1>
+					<Switch>
+						<Route path='/login' component={LoginForm} />
+						<Route path='/register' component={Create} />
+						<Route path='/' component={Home} />
+					</Switch>
+				</UserContext.Provider>
 			</TechContext.Provider>
 		</div>
 	);
