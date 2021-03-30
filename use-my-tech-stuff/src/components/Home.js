e61dbf9a036594665325c4f4110ebf61f369c37d
Aleksandr Ardanov <alex@air-aleksandr.lan>
2021-03-30 13:27:37 -0500

    removed route from index.js and cleaned up index.js

--- use-my-tech-stuff/src/components/Home.js
+++ use-my-tech-stuff/src/components/Home.js
@@ -5,7 +5,11 @@ import { TechContext } from '../contexts/techContext';
 const Home = () => {
 	const { tech } = useContext(TechContext);
 
-	return <div></div>;
+	console.log (tech)
+
+	return (
+	<div>
+	</div>);
 };
 
 export default Home;
