e822a07b758eda419ca79324135d838cada1ed06
Chris Burton <chris@fullstackcaveman.com>
2021-03-30 14:15:23 -0600

    Set up userLoggedIn Context - Added to Home.js

--- use-my-tech-stuff/src/App.js
+++ use-my-tech-stuff/src/App.js
@@ -10,9 +10,27 @@ import CreateListing from './components/CreateListing';
 
 import './App.css';
 import axios from 'axios';
+import { UserContext } from './contexts/userContext';
 
 function App() {
 	const [tech, setTech] = useState([]);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	// Quick way to set state without knowing what localStorage item will be called - may change later
+	useEffect(() => {
+		if (!localStorage.token) {
+			localStorage.setItem('token', '');
+		}
+
+		const userStatus = localStorage.getItem('token');
+		if (!userStatus) {
+			setIsLoggedIn(false);
+		} else {
+			setIsLoggedIn(true);
+		}
+	}, [isLoggedIn]);
+
+	console.log(isLoggedIn);
 
 	useEffect(() => {
 		axios
@@ -28,14 +46,16 @@ function App() {
 	return (
 		<div className='app'>
 			<TechContext.Provider value={{ tech }}>
-				<Nav />
-				<h1>Use My Tech Stuff</h1>
-				<Switch>
-					<Route path='/createlisting' component={CreateListing} />
-					<Route path='/login' component={LoginForm} />
-					<Route path='/register' component={Create} />
-					<Route path='/' component={Home} />
-				</Switch>
+				<UserContext.Provider value={{ isLoggedIn }}>
+					<Nav />
+					<h1>Use My Tech Stuff</h1>
+					<Switch>
+						<Route path='/createlisting' component={CreateListing} />
+						<Route path='/login' component={LoginForm} />
+						<Route path='/register' component={Create} />
+						<Route path='/' component={Home} />
+					</Switch>
+				</UserContext.Provider>
 			</TechContext.Provider>
 		</div>
 	);
--- use-my-tech-stuff/src/components/Card.js
+++ use-my-tech-stuff/src/components/Card.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../contexts/userContext';
 
-const Card = props => {
-    const {card} = props;
-    
-    return (
-        <div className='card'>
-            <h3>Item: {card.item_name}</h3>
-            <p>Price: {card.item_price}</p>
-            <p>Owner: {card.item_owner}</p>
-            <p>Available: {card.item_available ? 'Yes' : 'No'}</p>
-        </div>
-    )
-}
+const owner = false;
+
+const Card = (props) => {
+	const { isLoggedIn } = useContext(UserContext);
+	const { card } = props;
+
+	return (
+		<div className='card'>
+			<h3>Item: {card.item_name}</h3>
+			<p>Price: {card.item_price}</p>
+			<p>Owner: {card.item_owner}</p>
+			<p>Available: {card.item_available ? 'Yes' : 'No'}</p>
+			{owner && (
+				<div className='ownerButtons'>
+					<button>edit</button>
+					<button>delete</button>
+				</div>
+			)}
+			{isLoggedIn && (
+				<div className='userButtons'>
+					<button disabled={card.item_available ? false : true}>add</button>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default Card;
